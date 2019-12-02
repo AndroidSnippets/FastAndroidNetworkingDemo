1e1be0677c30e91cb2a4d1265d9ede5b6eb07eb7
Abdulsalam Mansour <abdulsalam.mans96@gmail.com>
2019-12-03 01:20:10 +0200

    add interceptor to debug

--- app/src/main/java/com/example/fastandroidnetworkingdemo/MainActivity.java
+++ app/src/main/java/com/example/fastandroidnetworkingdemo/MainActivity.java
@@ -3,22 +3,38 @@ package com.example.fastandroidnetworkingdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.Priority;
 import com.androidnetworking.error.ANError;
+import com.androidnetworking.interceptors.HttpLoggingInterceptor;
 import com.androidnetworking.interfaces.JSONArrayRequestListener;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AndroidNetworking.initialize(getApplicationContext());
 
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().connectTimeout(2, TimeUnit.MINUTES)
+                .readTimeout(2, TimeUnit.MINUTES)
+                .writeTimeout(2, TimeUnit.MINUTES).addInterceptor(interceptor).build();
+
+        AndroidNetworking.initialize(getApplicationContext(),client);
 
         AndroidNetworking.get("https://api.apis.guru/v2/list.json")
                 //.addPathParameter("pageNumber", "0")
@@ -31,11 +47,18 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONArray response) {
                         // do anything with response
+                        try {
+                            final JSONObject jsonObject = response.getJSONObject(0);
+                            Log.d(TAG, "onResponse: ");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
 
                     }
                     @Override
                     public void onError(ANError error) {
                         // handle error
+                        Log.d(TAG, "onError: ");
                     }
                 });
 
